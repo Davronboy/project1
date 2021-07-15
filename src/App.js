c91e61c44369b591e5f690ce7b5b53aece014b4d
Davronboy <davrontoxirjonov@gmail.com>
2021-07-15 17:02:25 +0300

    cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container py-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="bg-white shadow pb-4 text-center">
+              <img src="https://citatyok.ru/wp-content/uploads/2018/12/ocean_blue_sea_sand_waves_pacific_sea_water_blue-698420.jpgd_.jpg" className="w-100 mb-3" alt="" />
+              <h1 className="fw-bold mx-4">Blue Ocean Waves Crash</h1>
+              <p className="text-secondary mx-4"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi ipsam ea velit dolor, autem culpa ad cum aliquid itaque sequi amet distinctio mollitia vitae, aspernatur porro eos quod, incidunt ex!</p>
+            </div>
+
+          </div>
+          <div className="col-md-4">
+            <div className="bg-white shadow pb-4 text-center">
+              <img src="https://citatyok.ru/wp-content/uploads/2018/12/ocean_blue_sea_sand_waves_pacific_sea_water_blue-698420.jpgd_.jpg" className="w-100 mb-3" alt="" />
+              <h1 className="fw-bold mx-4">Blue Ocean Waves Crash</h1>
+              <p className="text-secondary mx-4"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi ipsam ea velit dolor, autem culpa ad cum aliquid itaque sequi amet distinctio mollitia vitae, aspernatur porro eos quod, incidunt ex!</p>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
